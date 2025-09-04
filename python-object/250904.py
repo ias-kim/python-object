96117537ebc0e0f67ee7902b997ab038c862310a
Theunkilabledemonking <abcqkdnxm@g.yju.ac.kr>
2025-09-04 10:53:50 +0900

    python object tutorial

--- python-object/250904.py
+++ python-object/250904.py
@@ -5,7 +5,21 @@ class A(object):
     # 소멸자
     pass
 class Foo():
-    def test(instance_ref):
-        instance_ref.name = "Foo"
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
 
-obj = Foo()
+obj = Foo("hong", 12)
+
+obj.university = "yju"
+
+import types
+def prt_unif (self, univ):
+    self.univ = univ
+obj.prt_univ = types.MethodType(prt_unif, obj)
+
+print(obj.name)
+print(obj.age)
+
+del obj.name
+print(obj.name)
