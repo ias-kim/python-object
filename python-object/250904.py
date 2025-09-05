bf17cce70a6fb087e334da3b05f0d2c8835b9722
Theunkilabledemonking <abcqkdnxm@g.yju.ac.kr>
2025-09-05 17:59:49 +0900

    Add python object contractor

--- python-object/250904.py
+++ python-object/250904.py
@@ -16,10 +16,11 @@ obj.university = "yju"
 import types
 def prt_unif (self, univ):
     self.univ = univ
-obj.prt_univ = types.MethodType(prt_unif, obj)
+
+obj.prt_univ = types.MethodType(prt_unif, obj) # 동적으로 메소드를 추가 가능하나 권장 X
 
 print(obj.name)
 print(obj.age)
 
 del obj.name
-print(obj.name)
+print(obj.name) # name 속성 접근 불가 권장 X
