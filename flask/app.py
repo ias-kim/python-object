1f7cd4c589bb71b280042709084654244e737e28
Theunkilabledemonking <abcqkdnxm@g.yju.ac.kr>
2025-11-21 17:51:40 +0900

    flask view 함수의 동작 방식

--- flask/app.py
+++ flask/app.py
@@ -1,15 +1,32 @@
-from flask import Flask
+from flask import Flask, request
 
 # Flask 클래스의 객체 생성
 # __name__: 현재 실행 프로그램의 경로 정보를 먼저 인자값으로 전달
 app = Flask(__name__) # 실행할 때에 현재 파일의 route라고 적시
 
 # Routing -> view function
-@app.route('/')
+@app.route('/', methods=["GET"])
 
 def home():
     # response
-    return "hello flask", 200, {"X-Param": "gsc"}
+    print(f"Path: {request.path}") # http request가 올때 생성이 되며, 해당 정보가 요청이 당함.
+    print(f"Method: {request.method}") # 그 헤더에 대한 정보 Response 클래스 객체를 남길 수 있다.
+    
+    upload_file_name = None
+
+    print(request.headers.get('gsc'))
+
+    file_obj = request.files.get("file_1")
+    if file_obj:
+        upload_file_name = file_obj.filename
+
+    return {
+        "method": request.method,
+        "param": request.args,
+        "json": request.get_json(silent=True),
+        "files": upload_file_name,
+        "forms": request.form
+    }
 
 print(app.url_map)
 
---
+++ flask/test.py
@@ -0,0 +1,28 @@
+from flask import Flask, Response
+
+app = Flask(__name__)
+
+@app.before_request
+def prt_log():
+    print("log")
+
+# Path : /, Method: Get or Post
+@app.route("/", methods=["GET", "POST"])
+def home():
+    return "hello flask", 200, {"GSC": "gsc"}
+
+@app.route("/student/<int:id>", methods=["GET"])
+def student(id):
+    return f"Student ID: {id}"
+
+@app.after_request
+def prt_after(rsp: Response):
+    print(rsp.headers.get('GSC'))
+    return rsp
+
+@app.teardown_request
+def teardown(e:Exception):
+    print(e)
+
+if __name__ == "__main__":
+    app.run(debug=True)
